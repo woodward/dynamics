aa228cb3d354d87972fca7ed049b2022cd81eae7
Greg Woodward <woodward@users.noreply.github.com>
2024-02-04 07:48:15 -0800

    Output of 'mix new dynamics'

---
+++ lib/dynamics.ex
@@ -0,0 +1,18 @@
+defmodule Dynamics do
+  @moduledoc """
+  Documentation for `Dynamics`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dynamics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dynamics_test.exs
@@ -0,0 +1,8 @@
+defmodule DynamicsTest do
+  use ExUnit.Case
+  doctest Dynamics
+
+  test "greets the world" do
+    assert Dynamics.hello() == :world
+  end
+end
